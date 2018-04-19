2710c1c773852440faf0b7b2d8292ba78a93fc52
Bayu lalu <bayulalu3@gmail.com>
2018-04-19 20:38:04 +0700

    tesu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Ter
                 </div>
 
                 <div class="links">
